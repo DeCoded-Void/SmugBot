14a73537a82ab8f088f79379733c058b80d4c257
DeCoded-Void <DeCoded-Void@users.noreply.github.com>
2020-04-12 21:15:22 -0400

    Update artifact.py
    updated regular expression and added a failsafe for when a user gives a wrong query.

--- cog/command/artifact.py
+++ cog/command/artifact.py
@@ -1,6 +1,7 @@
 import aiohttp
 import discord
 from discord.ext import commands
+from difflib import SequenceMatcher
 import random
 import re
 
@@ -21,7 +22,9 @@ class artifacts(commands.Cog):
                 if read.status == 200:
                     html = await read.text()
         html = html.replace('\n','')
-        pattern = '<tr><th><div class="center"><div class="floatnone"><a href="\/File(?:.*?)src="(.*?)"(?:.*?)<\/div><\/div>(.*?)<\/th><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><\/tr>'
+
+        pattern = '<tr><td><(?:.*?)<\/span>(?:.*?)src="(.*?)"(?:.*?)<td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><\/tr>'
+
         thelist = []
         for result in re.findall(pattern, html):
           thelist.append(list(result))
@@ -30,21 +33,38 @@ class artifacts(commands.Cog):
             if thelist[x][y] == '':
               thelist[x][y] = '\U0000FEFF'
             thelist[x][y] = thelist[x][y].replace('<i>','*').replace('</i>','*')
+            thelist[x][y] = re.sub('(<(.*?)>)','',thelist[x][y])
+        
         artifactmatch = None
         for x in thelist:
             if artifacttext.lower() == x[1].lower() or artifacttext.lower() == x[1].lower().replace(' ',''):
                 artifactmatch = x
 
         if artifactmatch is None:
-            await msg.edit(content=f'{ctx.author.mention}, I cannot find that artifact in the wiki...')
+            allnames = []
+            for possible in thelist:
+                allnames.append(possible[1])
+            for x in range(len(allnames)):
+                ratio = SequenceMatcher(None, allnames[x].lower(), artifacttext.lower()).ratio()
+                ratio = int(ratio*10000)
+                allnames[x] = [ratio, allnames[x]]
+            allnames.sort()
+            allnames.reverse()
+            allnames = allnames[:3]
+            possiblestr = ''
+            for x in allnames:
+                possiblestr += f'{x[1]}\n'
+            embed = discord.Embed(title='Did you mean?')
+            embed.add_field(name='Possible Artifacts', value=possiblestr, inline=True)
+            await msg.edit(content=f'{ctx.author.mention}, I cannot find that artifact in the wiki...', embed=embed)
             return
 
         embed = discord.Embed(title=artifactmatch[1], description=artifactmatch[3])
         if artifactmatch[0] != '\U0000FEFF':
             embed.set_thumbnail(url=artifactmatch[0])
         embed.add_field(name='Rarity', value=artifactmatch[4])
-        embed.add_field(name='Effect', value=artifactmatch[2],inline=False)
-        embed.set_footer(text=f'Tags: {artifactmatch[5]}')
+        embed.add_field(name='Description', value=artifactmatch[2],inline=False)
+        embed.set_footer(text=f'ID: {artifactmatch[6]} | Tags: {artifactmatch[5]}')
         await msg.edit(content=None,embed=embed)
 
 def setup(bot):
